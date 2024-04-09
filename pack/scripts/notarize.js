368f47a252864804191742c4cb356acf867c4af0
qishibo <shiboqi123@gmail.com>
2024-04-09 21:22:12 +0800

    ttt

--- pack/scripts/notarize.js
+++ pack/scripts/notarize.js
@@ -13,5 +13,6 @@ exports.default = async function notarizing(context) {
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEID_PASSWORD,
+    teamId: '68JN8DV835',
   });
 };
